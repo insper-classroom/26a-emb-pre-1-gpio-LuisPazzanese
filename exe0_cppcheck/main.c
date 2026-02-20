181f00c8c2376c6bf7a7e06b1fbf63b8ebd92ade
LuisPazzanese <luis@observer.localdomain>
2026-02-20 08:50:03 -0300

    exe0 3 try

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -9,13 +9,10 @@ int multiplyNumbers(int x, int y) {
 }
 
 int main(void) {
-    int a = 5, b = 5; //uninitvar, legacyUninitvar
+    int a = NUM_ELEMENTS;
+    int b = NUM_ELEMENTS; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
-    int arr[NUM_ELEMENTS];
-    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
-        arr[i] = i;
-    }
-
+    printf("Result: %d\n", result);
     return 0;
 }
