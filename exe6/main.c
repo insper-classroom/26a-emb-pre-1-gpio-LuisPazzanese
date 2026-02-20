76ea0ec14b58d4ce2b4a249ab1c21a355a33d211
LuisPazzanese <luifpp1@al.insper.edu.br>
2026-02-20 18:08:55 -0300

    exe6 3 try

--- exe6/main.c
+++ exe6/main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
 const int FIRST_GPIO = 2;
-const int BTN_PIN_G = 28;
+const int BTN_PIN = 28;
 
-int BUTTON_GPIO;
-int cnt;
-int last_btn; // Button not pressed (pulled up)
+int cnt = 0;
+int last_btn = 1; // Button not pressed (pulled up)
 
-// This array converts a number 0-9 to a bit pattern to send to the GPIOs
 int bits[10] = {
     0x3f,  // 0
     0x06,  // 1
@@ -34,32 +31,29 @@ void seven_seg_init() {
 void seven_seg_display() {
     int value = bits[cnt];
     for (int i = 0; i < 7; i++) {
-        int gpio = FIRST_GPIO + i;
         int bit = (value >> i) & 1;
-        gpio_put(gpio, bit);
+        gpio_put(FIRST_GPIO + i, bit);
     }
 }
 
 int main() {
     stdio_init_all();
 
-    gpio_init(BTN_PIN_G);
-    gpio_set_dir(BTN_PIN_G, GPIO_IN);
-    gpio_pull_up(BTN_PIN_G);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
     seven_seg_init();
     seven_seg_display();
 
     while (true) {
-        int btn = gpio_get(BTN_PIN_G);
-        if (last_btn && !btn) { // Detect falling edge (press)
-            if (++cnt > 9) {
-                cnt = 0;
-            }
+        int btn = gpio_get(BTN_PIN);
+        if (last_btn && !btn) {
+            if (++cnt > 9) cnt = 0;
             seven_seg_display();
             printf("cnt: %d\n", cnt);
         }
         last_btn = btn;
-        sleep_ms(10); // Polling interval
+        sleep_ms(10);
     }
 }
