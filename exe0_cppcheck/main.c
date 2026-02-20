691de0e535c0d35348c2560651886968db9632df
LuisPazzanese <luis@observer.localdomain>
2026-02-20 08:51:44 -0300

    exe0 5 try

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_ELEMENTS 5
-
 int multiplyNumbers(int x, int y) {
     int product = x * y;
     return product;
 }
 
 int main(void) {
-    int a = NUM_ELEMENTS;
-    int b = NUM_ELEMENTS; //uninitvar, legacyUninitvar
+    int a = 5;
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     printf("Result: %d\n", result);
